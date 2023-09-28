2006aa77657d842dd03b0ead4fa4d18e88f822fc
Harsha Peteti <petetiharsha2002@gmail.com>
2023-09-28 00:47:25 -0400

    Brought PBudget code in

---
+++ src/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  Link,
+} from "react-router-dom";
+
+function Menu() {
+    return (
+  <nav class="menu" aria-label="Main Navigation"> 
+      <ul>
+          <li><Link to="/">Homepage</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/login">Login</Link></li>
+      </ul>
+  </nav>
+    );
+  }
+  
+  export default Menu;
